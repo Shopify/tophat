9bb39ea19bc037ae694bf2cb095128196f3b53b4
Doan Thieu <thieurom@gmail.com>
2025-12-23 19:29:59 +0700

    Create TophatGitHubExtension target

---
+++ TophatExtensions/TophatGitHubExtension/TophatGitHubExtension.swift
@@ -0,0 +1,17 @@
+//
+//  TophatGitHubExtension.swift
+//  TophatGitHubExtension
+//
+//  Created by Doan Thieu on 23/12/25.
+//  Copyright © 2025 Shopify. All rights reserved.
+//
+
+import Foundation
+import TophatKit
+import SwiftUI
+internal import ExtensionFoundation
+
+@main
+struct TophatGitHubExtension: TophatExtension {
+    static let title: LocalizedStringResource = "GitHub"
+}
