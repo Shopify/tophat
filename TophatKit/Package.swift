08d7a10cd6ff2d5e5fa19d7a05a3a64ce8881052
Doan Thieu <thieurom@gmail.com>
2025-12-24 11:05:04 +0700

    Make SecureStorage as a module of TophatKit

--- TophatKit/Package.swift
+++ TophatKit/Package.swift
@@ -5,16 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "TophatKit",
-	platforms: [
-		.macOS(.v14)
-	],
+    platforms: [
+        .macOS(.v14)
+    ],
     products: [
         .library(
             name: "TophatKit",
             targets: ["TophatKit"]
-		    ),
+        ),
+        .library(
+            name: "SecureStorage",
+            targets: [
+                "SecureStorage"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/auth0/SimpleKeychain.git", .upToNextMajor(from: "1.2.0"))
     ],
     targets: [
-        .target(name: "TophatKit")
+        .target(name: "TophatKit"),
+        .target(
+            name: "SecureStorage",
+            dependencies: [
+                .product(name: "SimpleKeychain", package: "SimpleKeychain")
+            ]
+        )
     ]
 )
--- TophatKit/Sources/SecureStorage/SecureStorage.swift
+++ TophatKit/Sources/SecureStorage/SecureStorage.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import SimpleKeychain
 
 @propertyWrapper
-struct SecureStorage {
+public struct SecureStorage {
 	private let key: String
 	private let keychain = SimpleKeychain()
 
