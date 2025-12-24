631b4511402d7cfbc91b827ba8ac1c3453f2b4f7
Doan Thieu <thieurom@gmail.com>
2025-12-24 10:08:11 +0700

    Download artifact from GitHub Action

---
+++ TophatExtensions/TophatGitHubExtension/ArtifactProviders/GitHubActionArtifactProvider.swift
@@ -0,0 +1,50 @@
+//
+//  GitHubActionArtifactProvider.swift
+//  TophatGitHubExtension
+//
+//  Created by Doan Thieu on 23/12/25.
+//  Copyright © 2025 Shopify. All rights reserved.
+//
+
+import Foundation
+import TophatKit
+
+struct GitHubActionArtifactProvider: ArtifactProvider {
+    static var id = "github-action"
+    static var title: LocalizedStringResource = "GitHub Action"
+
+    @Parameter(key: "owner", title: "Owner")
+    var owner: String
+
+    @Parameter(key: "repo", title: "Repository")
+    var repository: String
+
+    @Parameter(key: "artifact_id", title: "Artifact Id")
+    var artifactId: String
+
+    private let apiClient = GitHubActionAPIClient(personalAccessToken: "")
+    private var urlSession = URLSession.shared
+    private let fileManager = FileManager.default
+
+    func retrieve() async throws -> any ArtifactProviderResult {
+        let (downloadedFileURL, suggestedFilename) = try await apiClient.downloadArtifact(
+            owner: owner,
+            repository: repository,
+            artifactId: artifactId
+        )
+
+        let destinationDirectoryURL: URL = .temporaryDirectory.appending(path: UUID().uuidString)
+        try fileManager.createDirectory(at: destinationDirectoryURL, withIntermediateDirectories: true)
+
+        let destinationURL = destinationDirectoryURL
+            .appending(component: suggestedFilename ?? downloadedFileURL.lastPathComponent)
+
+        try fileManager.moveItem(at: downloadedFileURL, to: destinationURL)
+
+        return .result(localURL: destinationURL)
+    }
+
+    func cleanUp(localURL: URL) async throws {
+        try fileManager.removeItem(at: localURL)
+    }
+}
