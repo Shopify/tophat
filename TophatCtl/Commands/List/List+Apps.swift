a9f237163ab4ef90d75f5a6215c6ba0b9f14582e
Lukas Romsicki <lukas.romsicki@shopify.com>
2026-03-13 16:01:22 -0400

    Add json support for other listing commands

--- TophatCtl/Commands/List/List+Apps.swift
+++ TophatCtl/Commands/List/List+Apps.swift
@@ -8,6 +8,7 @@
 
 import ArgumentParser
 import TophatControlServices
+import Foundation
 
 extension List {
 	struct Apps: AsyncParsableCommand {
@@ -15,21 +16,30 @@ extension List {
 			abstract: "Lists all apps configured for Quick Launch in Tophat."
 		)
 
+		@Flag(name: .long, help: "Output the result as JSON.")
+		var json = false
+
 		func run() async throws {
 			checkIfHostAppIsRunning()
 
 			let reply = try await TophatRemoteControlService().send(request: ListAppsRequset())
 
-			print("Apps:")
+			if json {
+				let encoder = JSONEncoder()
+				encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+				let data = try encoder.encode(reply.apps)
+				print(String(data: data, encoding: .utf8)!)
+			} else {
+				print("Apps:")
 
-			for app in reply.apps {
-				print()
+				for app in reply.apps {
+					print()
 
-				print("‣\u{001B}[1m", app.name, "\u{001B}[22m")
-				print("  Identifier:", app.id)
-				print("  Platforms:", app.platforms.map { String(describing: $0) }.joined(separator: ", "))
-				print("  Recipes:", app.recipeCount)
-
+					print("‣\u{001B}[1m", app.name, "\u{001B}[22m")
+					print("  Identifier:", app.id)
+					print("  Platforms:", app.platforms.map { String(describing: $0) }.joined(separator: ", "))
+					print("  Recipes:", app.recipeCount)
+				}
 			}
 		}
 	}
